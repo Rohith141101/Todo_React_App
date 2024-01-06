13a80d4103daf435104040bb356a80f817d2ed71
Rohith <73777100+Rohith141101@users.noreply.github.com>
2024-01-06 22:13:14 +0530

    Update TodoWrapper.js

--- src/Components/TodoWrapper.js
+++ src/Components/TodoWrapper.js
@@ -1,17 +1,23 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import TodoForm from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
 import Todo from './Todo'
 import EditTodoForm from './EditTodoForm'
 uuidv4()
+const getItemsromStorage=()=>{
+    const task=localStorage.getItem("tasks")
+    if(task){
+        return JSON.parse(task)
+    }
+    else{
+        return []
+    }
+}
 const TodoWrapper = () => {
-    const[todos,setTodos]=useState([{id:uuidv4(), task:"Need to create a portfolio", completed:false, isEditing:false},
-                                   {id:uuidv4(), task:"Need to create a website", completed:false, isEditing:false },
-                                  {id:uuidv4(), task:"Need to create a API", completed:false, isEditing:false}])
+    const[todos,setTodos]=useState(getItemsromStorage())
 
     const addTodo=(todo)=>{
         setTodos([...todos, {id:uuidv4(), task:todo, completed:false, isEditing:false }])
-        console.log(todos)
     }
     const toggleComplete=(id)=>{
         setTodos(todos.map(todo=>todo.id===id?{
@@ -22,6 +28,7 @@ const TodoWrapper = () => {
     }
     const deleteTodo=(id)=>{
         setTodos(todos.filter((todo)=>todo.id!==id))
+        localStorage.removeItem(todos.filter((todo)=>todo.id!==id))
     }
     const editTodo=(id)=>{
         setTodos(todos.map((todo)=>todo.id===id?{
@@ -31,6 +38,10 @@ const TodoWrapper = () => {
     }
     const editTask=(task,id)=>{
         setTodos(todos.map(todo=>todo.id===id?{...todo,task,isEditing:!todo.isEditing}:todo))}
+
+    useEffect(()=>{
+        localStorage.setItem("tasks",JSON.stringify(todos))
+    },[todos])
   return (
     <div className='TodoWrapper'>
         <h1> Todo App</h1>
